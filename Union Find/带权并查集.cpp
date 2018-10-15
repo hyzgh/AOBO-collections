74888048b8e81484d644f0ca138bc4a90876d599
zhangab <2411035458@qq.com>
2018-10-15 20:58:22 +0800

    带权并查集

---
+++ Union Find/带权并查集.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 2e5+5;
+
+struct pre
+{
+	int p[N], dis[N];
+	void init(int n)
+	{
+		memset(dis, 0, sizeof dis);
+		for(int i = 0; i <= n; i ++)
+			p[i] = i;
+	}
+	int Find(int x)
+	{
+		if(x == p[x])	return p[x];
+		int fa = Find(p[x]);
+		dis[x] += dis[p[x]];
+		return p[x] = fa;
+	}
+	void Mix(int x, int y ,int dis_xy)
+	{
+		int fx = Find(x), fy = Find(y);
+		if(fx==fy)	return ;
+		p[fy] = fx;
+		dis[fy] = dis[x] + dis_xy - dis[y];
+	}
+};
+
+
+

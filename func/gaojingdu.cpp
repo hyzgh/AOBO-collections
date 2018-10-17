934e9d92e849a1077fdc2e3c9c80405fc842835e
hyzgh <31174102+hyzgh@users.noreply.github.com>
2018-10-17 20:24:33 +0800

    添加高精度模板

---
+++ func/gaojingdu.cpp
@@ -0,0 +1,71 @@
+/************************************************************/
+/* 高精度，支持乘法和加法(Tested 1 times)
+ */
+struct BigInt{
+	const static int mod = 10000;
+	const static int DLEN = 4;
+	int a[600],len;		//len × DLEN为总长度，可以修改一下a数组的大小，防止MLE
+	BigInt(){
+		memset(a,0,sizeof(a));
+		len = 1;
+	}
+	BigInt(int v){
+		memset(a,0,sizeof(a));
+		len = 0;
+		do{
+			a[len++] = v%mod;
+			v /= mod;
+		}while(v);
+	}
+	BigInt(const char s[]){
+		memset(a,0,sizeof(a));
+		int L = strlen(s);
+		len = L/DLEN;
+		if(L%DLEN)len++;
+		int index = 0;
+		for(int i = L-1;i >= 0;i -= DLEN){
+			int t = 0;
+			int k = i - DLEN + 1;
+			if(k < 0)k = 0;
+			for(int j = k;j <= i;j++)
+				t = t*10 + s[j] - '0';
+			a[index++] = t;
+		}
+	}
+	BigInt operator +(const BigInt &b)const{
+		BigInt res;
+		res.len = max(len,b.len);
+		for(int i = 0;i <= res.len;i++)
+			res.a[i] = 0;
+		for(int i = 0;i < res.len;i++){
+			res.a[i] += ((i < len)?a[i]:0)+((i < b.len)?b.a[i]:0);
+			res.a[i+1] += res.a[i]/mod;
+			res.a[i] %= mod;
+		}
+		if(res.a[res.len] > 0)res.len++;
+		return res;
+	}
+	BigInt operator *(const BigInt &b)const{
+		BigInt res;
+		for(int i = 0; i < len;i++){
+			int up = 0;
+			for(int j = 0;j < b.len;j++){
+				int temp = a[i]*b.a[j] + res.a[i+j] + up;
+				res.a[i+j] = temp%mod;
+				up = temp/mod;
+			}
+			if(up != 0)
+				res.a[i + b.len] = up;
+		}
+		res.len = len + b.len;
+		while(res.a[res.len - 1] == 0 &&res.len > 1)res.len--;
+		return res;
+	}
+	void output(){
+		printf("%d",a[len-1]);
+		for(int i = len-2;i >=0 ;i--)
+			printf("%04d",a[i]);
+		printf("\n");
+	}
+};
+/************************************************************/
